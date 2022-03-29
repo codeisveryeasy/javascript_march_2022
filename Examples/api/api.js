4ef6fb71dc05c90df5fee7a7ab30fdac6fc32883
codeisveryeasy <89445533+codeisveryeasy@users.noreply.github.com>
2022-03-29 18:47:36 +0530

    API

---
+++ Examples/api/api.js
@@ -0,0 +1,44 @@
+console.log("API")
+
+var callapiref = document.querySelector("#callapi")
+callapiref.addEventListener("click", getData)
+var users = []
+var usersref = document.querySelector("#users")
+console.log(usersref)
+
+async function getData(){
+    
+    let response  = await fetch("https://jsonplaceholder.typicode.com/users")
+    console.log(response)
+    console.log(response.status)
+    if(response.status == 200){
+        let data = await response.json()
+        users = data
+        console.log(users)
+        console.log(users[0])
+        console.log(users[1])
+        console.log(users[2])
+        console.log(users[3])
+        console.log(users[4])
+        console.log("Length of data in response: " + data.length)
+
+        //display users in document
+        for (let index = 0; index < users.length; index++) {
+            var newli = document.createElement("li")
+            newli.innerText = users[index].name + " @ id: " + users[index].id
+            usersref.appendChild(newli)
+            
+        }
+       
+
+    }
+
+}
+
+
+
+
+
+
+
+//getData()
